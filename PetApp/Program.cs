469e5db772bde41cef86c9af83cf54fa95eaac46
jgrudnoff <jgrudnoff@gmail.com>
2024-10-18 20:51:17 +1000

    Use constant for api url

--- PetApp/Program.cs
+++ PetApp/Program.cs
@@ -11,11 +11,13 @@ namespace PetApp
 {
     class Program
     {
+        const string API_ROOT = "https://petstore.swagger.io/v2/";
+
         static async Task Main(string[] args)
         {
             try
             {
-                var petStoreApi = RestService.For<IPetStoreApi>("https://petstore.swagger.io/v2");
+                var petStoreApi = RestService.For<IPetStoreApi>(API_ROOT);
                 var petService = new PetRetrievalService(petStoreApi);
                 PetConsolePrintingService petPrintingService = new PetConsolePrintingService(petService);
                 List<PetStatus> statusList = new List<PetStatus>() { PetStatus.Available };
